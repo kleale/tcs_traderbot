552f0e266647858dadf3171a35419d10331f5c74
Alex <klealekle@gmail.com>
2021-01-20 22:12:45 +0300

    index_bak

---
+++ src/config/index_bak.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // secret: 'budgetsecret',
+    // session: { session: false },
+    // database: 'mongodb://127.0.0.1:27017/budgetmanager'
+    apiURL: 'https://api-invest.tinkoff.ru/openapi',
+    sandboxApiURL: "https://api-invest.tinkoff.ru/openapi/sandbox/",
+    socketURL: "wss://api-invest.tinkoff.ru/openapi/md/v1/md-openapi/ws",
+    secretToken: "", // токен для боевого api
+    sandboxToken:
+      ""
+  }
